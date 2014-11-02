44f6f805dbe279fc2a91263d7baeddc6d80f2d5c
Daniel X Moore <yahivin@gmail.com>
2014-11-01 19:04:09 -0700

    Reduced client connections

--- sidekiq.rb
+++ sidekiq.rb
@@ -14,7 +14,7 @@ redis_conn = proc {
 }
 
 Sidekiq.configure_client do |config|
-  config.redis = ConnectionPool.new(size: 4, &redis_conn)
+  config.redis = ConnectionPool.new(size: 2, &redis_conn)
 end
 Sidekiq.configure_server do |config|
   config.redis = ConnectionPool.new(size: 2, &redis_conn)
