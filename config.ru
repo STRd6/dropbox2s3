d5874efcd9e4cdbab23df03c2d06b6e6743af453
Daniel X Moore <yahivin@gmail.com>
2014-11-01 18:59:49 -0700

    Configuring sidekiq connections

--- config.ru
+++ config.ru
@@ -7,3 +7,14 @@ run Rack::URLMap.new(
   '/' => Sinatra::Application,
   '/sidekiq' => Sidekiq::Web
 )
+
+redis_conn = proc {
+  Redis.new
+}
+
+Sidekiq.configure_client do |config|
+  config.redis = ConnectionPool.new(size: 2, &redis_conn)
+end
+Sidekiq.configure_server do |config|
+  config.redis = ConnectionPool.new(size: 2, &redis_conn)
+end
--- sidekiq.rb
+++ sidekiq.rb
@@ -9,4 +9,13 @@ S3_SECRET = ENV['S3_SECRET']
 
 TOKEN = ENV['DROPBOX_TOKEN']
 
-REDIS = Redis.new(:url => ENV['REDIS_URL'])
+redis_conn = proc {
+  Redis.new
+}
+
+Sidekiq.configure_client do |config|
+  config.redis = ConnectionPool.new(size: 4, &redis_conn)
+end
+Sidekiq.configure_server do |config|
+  config.redis = ConnectionPool.new(size: 2, &redis_conn)
+end
